381f066a287bc1b98932448343ebacbc03d3e83b
NikoKutateladze <140888630+NikoKutateladze@users.noreply.github.com>
2023-08-08 03:08:28 -0700

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,7 +1,8 @@
-rows=10
-columns=10
-    
-
-for i in range (1,rows+1) :
-    for j in range(1,columns+1) :
-        print(f"{i}x{j}")
+a=1
+if a<a:
+    a=a-1
+while a>0:
+    a=a+2
+    print(a)
+    while a>98:
+        a=a
