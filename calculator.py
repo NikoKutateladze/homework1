adf5e245ba2b7b295176ac32e62d1066979fec85
NikoKutateladze <140888630+NikoKutateladze@users.noreply.github.com>
2023-09-02 10:28:07 -0700

    Calculator

---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+number=float(input())
+number1=float(input())
+calculation=input("choose onptions (* or / or +)")
+
+if calculation=='*':
+    result= number * number1
+   
+
+elif calculation=="+":
+    result= number + number1
+  
+
+elif calculation=='/':
+    result= number / number1
+
+
+print(f'Your calculation of {number} {calculation} {number1} is {result}')
+
+
+
+
+
